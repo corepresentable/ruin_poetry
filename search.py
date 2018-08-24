7cb7f5bf84cb90ffecf0b87d3133d0257b0fb0d6
Joseph Hirsh <josephhirsh@gmail.com>
2018-08-24 11:32:09 -0500

    sorted

--- search.py
+++ search.py
@@ -1,8 +1,46 @@
 import gensim
+import numpy as np 
+import operator as op 
 
-model = gensim.models.KeyedVectors.load_word2vec_format('w2v', binary=True)
 
-L = ["cat", "dog", "fish", "space force"]
 
-print(model['dog'])
+
+#load the model (takes forever!!)
+
+model = gensim.models.KeyedVectors.load_word2vec_format('~/W2V/w2v', binary=True)
+
+#list of words
+L = ['cat', 'dog', 'space', 'ennervate', 'call']
+
+
+#initiate large numpy vector
+big = 10000
+
+#initiate a word vector
+v = model["telephone"]
+
+
+#add distances to wordlist
+def dModel(L, vector):
+	dlist = [],
+	for w in L:
+		try:
+			_vec = model[w]
+			_dist = np.linalg.norm(_vec - vector)
+			dlist.append((w, _dist))
+		except KeyError:
+			dlist.append((w, big))
+	return dlist
+
+def topN(dL,N):
+	topN = sorted(dL, key=op.itemgetter(1), reverse=True)[:N]
+	return topN
+
+
+
+
+
+
+
+
 
