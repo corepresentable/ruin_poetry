1769b615fc796817fa75aafedb0fcd19bd1dbf73
Joseph Hirsh <josephhirsh@gmail.com>
2018-09-29 17:02:48 -0500

    alliterative added

---
+++ alliterative.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+
+def first_sound(s):
+	i = s.find(" ")
+	return s[0:i]
+
+
+def create_allit(s):
+	allit = []
+	for adj in wF.itertuples():
+		if adj[2] == "A":
+			for noun in wF.itertuples():
+				if noun[3] == adj[3] == s:
+					if noun[1] != adj[1]:
+						if noun[2] == "N":
+							_allit_pair = adj[1] + " " + noun[1]
+							allit.append(_allit_pair)
+							print(_allit_pair)
+	return allit
+
+mobyFrame = pd.read_csv("mobyFrame")
+rhymeFrame = pd.read_csv("rhymeFrame")
+
+print("Moby size is:", mobyFrame.shape)
+print("rhyme size is:", rhymeFrame.shape)
+
+
+#fix type
+rhymeFrame['word'] = rhymeFrame['word'].astype(str)
+#remove numbers from pronunciation
+rhymeFrame['pron'] = rhymeFrame['pron'].apply(lambda x: ''.join([i for i in x if not i.isdigit()]))
+#first sound
+rhymeFrame['pron'] = rhymeFrame['pron'].apply(lambda x: first_sound(x))
+#fix type
+mobyFrame['word'] = mobyFrame['word'].astype(str)
+#change to uppercase
+mobyFrame['word'] = mobyFrame['word'].apply(lambda x: x.upper())
+
+
+
+wF = mobyFrame.join(rhymeFrame.set_index('word'), on='word')
+
+wF = wF[pd.notnull(wF['pos'])]
+
+wF = wF[pd.notnull(wF['pron'])]
+
+wF = wF.reset_index(drop=True)
+
+wF.columns = ['first' if x == 'pron' else x for x in wF.columns]
+
+print(wF.head())
+
+print("wordFrame size is:", wF.shape)
+
+phonemes = ["AA","AE","AH", "AO", "AW", "AY", "B", "CH", "D","DH","EH","ER","EY","F","G",
+"HH","IH","IY","JH","K","L","M","N","NG", "OW","OY","P","R","S","SH","T","TH","UH",
+"UW","V","W","Y","Z","ZH"]
+
+for phoneme in phonemes:
+	allit_list = create_allit(phoneme)
+
+exit_path = "allit" + "_" + phoneme + ".txt"
+
+outF = open(exit_path, 'w')
+for pair in allit_list:
+	outF.write(pair)
+	outF.write("\n")
+outF.close()
+
+
+
+					
+
+
+
+
+
---
+++ search_allit.py
@@ -0,0 +1,62 @@
+import pickle
+import numpy as np 
+import pandas as pd
+import operator as op 
+import gensim
+
+path = "allit_T.txt"
+adjNoun = pd.read_csv(path, sep="\n", header=None)
+L = adjNoun[0].tolist()
+print("The list", path, "has length", len(L), ".")
+
+
+model = gensim.models.KeyedVectors.load_word2vec_format('~/W2V/w2v', binary=True)
+
+
+#initiate large numpy vector
+big = 100
+
+
+#add distances to wordlist
+def dModel(vector):
+    dlist = []
+    for rhyme in L:
+        try:
+            w = rhyme
+            rhyme = rhyme.lower()
+            word = rhyme.split(" ")
+            _vec = ( model[word[0]] + model[word[1]] ) * 0.5
+            _dist = np.linalg.norm(_vec - vector)
+            dlist.append([w, _dist])
+        except KeyError:
+            dlist.append([w, big])
+    return dlist
+
+def topN(dL,N):
+    topN = sorted(dL, key=op.itemgetter(1), reverse=False)[:N]
+    return topN
+
+while True:
+    print("Enter a word:")
+    phrase = input().strip().split()
+    vec = np.zeros(300)
+    vecList = []
+    svecList = []
+    for word in phrase:
+        try:
+            vec = vec + model[word]
+        except KeyError:
+            print("'", word, "' is not currently in our vocabulary.")
+    vec = vec * (1/len(phrase))      
+    vecList = dModel(vec)
+    svecList = topN(vecList, 20)
+    for rhyme in svecList:
+        print(rhyme)
+
+
+
+
+
+
+
+
