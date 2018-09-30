9d5ccec8e9c94e8a8e0d69c80105b767a265c231
Joseph Hirsh <josephhirsh@gmail.com>
2018-09-29 20:02:35 -0500

    allit lists

--- alliterative.py
+++ alliterative.py
@@ -53,7 +53,7 @@ print(wF.head())
 
 print("wordFrame size is:", wF.shape)
 
-phonemes = ["AA","AE","AH", "AO", "AW", "AY", "B", "CH", "D","DH","EH","ER","EY","F","G",
+phonemes = ["AA", "AE","AH", "AO", "AW", "AY", "B", "CH", "D","DH","EH","ER","EY","F","G",
 "HH","IH","IY","JH","K","L","M","N","NG", "OW","OY","P","R","S","SH","T","TH","UH",
 "UW","V","W","Y","Z","ZH"]
 
